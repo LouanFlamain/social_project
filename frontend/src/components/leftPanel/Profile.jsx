684ae5621678c1f446f8f7c3008117e001f0b354
LouanFlamain <flamainlouan@gmail.com>
2023-10-11 00:27:12 +0200

    first commit

--- frontend/src/components/leftPanel/Profile.jsx
+++
@@ -1,8 +0,0 @@
-const Profile = ({ userImage }) => {
-  return (
-    <div className="w-10 rounded-full mr-4">
-      <img className="rounded-full" src={`pp/${userImage}.jpeg`} />
-    </div>
-  );
-};
-export default Profile;
