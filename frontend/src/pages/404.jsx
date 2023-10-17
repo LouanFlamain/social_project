cc8ba3b65a82d216776b546b00e921406b4b9f1f
LouanFlamain <flamainlouan@gmail.com>
2023-10-17 14:55:25 +0200

    add verif

--- frontend/src/pages/404.jsx
+++
@@ -1,4 +0,0 @@
-const Error404 = () => {
-  return <p>Cette page n'existe pas</p>;
-};
-export default Error404;
--- frontend/src/pages/homePage.jsx
+++
@@ -1,5 +0,0 @@
-const Homepage = () => {
-  return <section className="w-full h-screen flex p-8"></section>;
-};
-
-export default Homepage;
