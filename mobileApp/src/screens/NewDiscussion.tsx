24c218eb164700100f52c07e0fd06e550f1a0de2
marie <mje.espinosa@gmail.com>
2023-12-15 17:16:00 +0100

    init mobile app

---
+++ mobileApp/src/screens/NewDiscussion.tsx
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+import SearchBar from '../components/SearchBar';
+
+
+function NewDiscussion(): JSX.Element {
+
+  return (
+    <SafeAreaView >
+        <View>
+            
+        </View>
+      
+    </SafeAreaView>
+  );
+}
+
+
+export default NewDiscussion;
