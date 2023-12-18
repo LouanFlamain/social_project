4f7f1890628221639fd502c603e975d45b7731bd
Marie <mje.espinosa@gmail.com>
2023-12-18 11:28:32 +0100

    better use of navigation and security

--- mobileApp/App.tsx
+++ mobileApp/App.tsx
@@ -1,80 +1,16 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import Messages from './src/screens/Messages';
-import Settings from './src/screens/Settings';
-import NewDiscussion from './src/screens/NewDiscussion';
-import Login from './src/screens/Login';
-import Register from './src/screens/Register';
-
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { EnvelopeIcon as Message } from "react-native-heroicons/mini";
-import { PlusCircleIcon as Create } from "react-native-heroicons/mini";
-import { Cog6ToothIcon as SettingsIcon } from "react-native-heroicons/mini";
-import { createStackNavigator } from '@react-navigation/stack';
-import Conversation from './src/screens/Conversation';
-
-
-
-
-
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-
-
-function HomeTabs() {
-  return (
-    <Tab.Navigator initialRouteName="Messages"
-      screenOptions={{
-        tabBarShowLabel : false,
-        tabBarHideOnKeyboard:true,
-        tabBarStyle: {
-          height: 70,
-          borderTopWidth: 0,
-          backgroundColor: '#F2F2F2',
-        }
-      }}
-      
-      >
-        <Tab.Screen name="Messages" component={Messages} 
-        options={{
-          tabBarIcon: () => (
-            <Message color="red" />
-          ),
-          headerShown: false
-
-        }}/>
-        <Tab.Screen name="Créer" component={NewDiscussion}
-        options={{
-          tabBarIcon: () => (
-            <Create color="red" />
-          ),
-          headerShown: false
-        }} />
-        <Tab.Screen name="Settings" component={Settings} 
-        options={{
-          tabBarIcon: () => (
-            <SettingsIcon color="red" />
-          ),
-          headerShown: false
-        }}/>
-      </Tab.Navigator>
-  );
-}
-
+import { Provider } from 'react-redux'
+import { persistor, store } from './src/utils/redux/Store';
+import { PersistGate } from 'redux-persist/integration/react';
+import Navigation from './src/Navigation';
 
 export default function App() {
-  return (
-    <NavigationContainer>
 
-<Stack.Navigator screenOptions={{
-          headerShown: false, // Cela cache le header pour tous les écrans dans ce Stack.Navigator
-        }}>
-      <Stack.Screen name="Home" component={HomeTabs} />
-      <Stack.Screen name="Login" component={Login}/>
-      <Stack.Screen name="Register" component={Register} />
-      <Stack.Screen name="Conversation" component={Conversation}/>
-    </Stack.Navigator>
-      
-    </NavigationContainer>
+  return (
+    <Provider store={store}>
+    <PersistGate persistor={persistor} loading={null}>
+    <Navigation/>
+    </PersistGate>
+    </Provider>
   );
 }
---
+++ mobileApp/src/Navigation.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import Messages from './screens/Messages';
+import Settings from './screens/Settings';
+import NewDiscussion from './screens/NewDiscussion';
+import Login from './screens/Login';
+import Register from './screens/Register';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { EnvelopeIcon as Message } from "react-native-heroicons/mini";
+import { PlusCircleIcon as Create } from "react-native-heroicons/mini";
+import { Cog6ToothIcon as SettingsIcon } from "react-native-heroicons/mini";
+import { createStackNavigator } from '@react-navigation/stack';
+import Conversation from './screens/Conversation';
+import { useAppSelector } from './utils/redux/hook';
+import { selectIsLoggedIn } from './utils/redux/UserSlice';
+import { selectState } from './utils/redux/UserSlice';
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator initialRouteName="Messages"
+      screenOptions={{
+        tabBarShowLabel : false,
+        tabBarHideOnKeyboard:true,
+        tabBarStyle: {
+          height: 70,
+          borderTopWidth: 0,
+          backgroundColor: '#F2F2F2',
+        }
+      }}
+      
+      >
+        <Tab.Screen name="Messages" component={Messages} 
+        options={{
+          tabBarIcon: () => (
+            <Message color="red" />
+          ),
+          headerShown: false
+
+        }}/>
+        <Tab.Screen name="Créer" component={NewDiscussion}
+        options={{
+          tabBarIcon: () => (
+            <Create color="red" />
+          ),
+          headerShown: false
+        }} />
+        <Tab.Screen name="Settings" component={Settings} 
+        options={{
+          tabBarIcon: () => (
+            <SettingsIcon color="red" />
+          ),
+          headerShown: false
+        }}/>
+      </Tab.Navigator>
+  );
+}
+
+const Navigation = () => {
+    const isLoggedIn = useAppSelector(selectIsLoggedIn)
+    const state = useAppSelector(selectState)
+
+    console.log(state)
+
+
+    console.log(isLoggedIn)
+  return (
+    <NavigationContainer>
+
+<Stack.Navigator screenOptions={{
+          headerShown: false, // Cela cache le header pour tous les écrans dans ce Stack.Navigator
+        }}>
+      <Stack.Screen name="Home" component={isLoggedIn ? HomeTabs : Login} />
+      <Stack.Screen name="Login" component={Login}/>
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="Conversation" component={isLoggedIn ? Conversation : Login}/>
+    </Stack.Navigator>
+      
+    </NavigationContainer>
+  )
+}
+
+export default Navigation
