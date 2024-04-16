d6af54fb86be9dc8c1ea0ed2d8d7140296bd2c32
Emerson452 <emerson@popoyoko.fr>
2024-04-16 11:43:40 +0200

    ✨ Add 3D element in login page

--- frontend/src/components/login/login_info.jsx
+++ frontend/src/components/login/login_info.jsx
@@ -1,7 +1,10 @@
+import Spline from '@splinetool/react-spline';
+
 const LoginInfo = () => {
   return (
-    <div className="w-1/2 h-full bg-secondary hidden md:flex md:rounded-l-2xl"></div>
+    <div className="w-1/2 h-full bg-secondary hidden md:flex md:rounded-l-2xl">
+        <Spline scene="https://prod.spline.design/5zEKsSghg2YG5LHP/scene.splinecode" />
+    </div>
   );
 };
-
 export default LoginInfo;
