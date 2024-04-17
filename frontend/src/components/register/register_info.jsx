4791642c7b60aa92089327bcc31d297b57628c63
Emerson452 <emerson@popoyoko.fr>
2024-04-17 09:42:33 +0200

    ✨ Add a 3D view

--- frontend/src/components/register/register_info.jsx
+++ frontend/src/components/register/register_info.jsx
@@ -1,6 +1,11 @@
+import Spline from '@splinetool/react-spline';
+
+
 const RegisterInfo = () => {
   return (
-    <div className="w-1/2 h-full bg-secondary hidden md:flex md:rounded-r-2xl"></div>
+    <div className="w-1/2 h-full bg-secondary hidden md:flex md:rounded-r-2xl">
+      <Spline scene="https://prod.spline.design/5zEKsSghg2YG5LHP/scene.splinecode" />
+    </div>
   );
 };
 
